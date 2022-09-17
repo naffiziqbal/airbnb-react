1c38d02218fa6a4d0f6fa61980cdda4feed0ea5a
naffiziqbal <f.nafiziqbal@gmail.com>
2022-09-18 00:22:49 +0600

    dynamic Cards

--- src/App.js
+++ src/App.js
@@ -7,7 +7,31 @@ function App() {
   return (
     <>
     <NavBar />
-    <Card/>
+
+<div className='cards container'>
+<Card 
+    img = "./images/card-1.jpg"
+    name = "I dont Know"
+    price ="13"
+    rating = " 6"
+    country= "  USA"
+    />
+    <Card 
+    img = "./images/card-2.png"
+    name = "I dont Know"
+    price ="132"
+    rating = "21 6"
+    country= "  SA"
+    />
+    <Card 
+    img = "./images/card-3.jpg"
+    name = "I dont Know"
+    price ="44"
+    rating = " 26"
+    country= "  UK"
+    />
+</div>
+    
     </>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
   return (
-    <div className="cards">
       <div className="card">
-        <img src="./images/card-1.jpg" />
+        <img src={props.img} />
         <div className="rating">
-          <i class="fa-regular fa-star"></i> (6) USA
+          <span><i class="fa-regular fa-star"></i></span>
+          <span> {props.rating}</span>
+          <span>{ props.country}</span>
         </div>
-        <h2>Name of content</h2>
+        <h2>{props.name}</h2>
         <h3>
-          From $<span>13</span>
+          From $<span>{props.price}</span>
         </h3>
-      </div>
-      <div className="card">
+      {/* <div className="card">
         <img src="./images/card-2.png" />
         <div className="rating">
           <i class="fa-regular fa-star"></i> (6) USA
@@ -32,7 +32,7 @@ export default function Card() {
         <h3>
           From $<span>13</span>
         </h3>
-      </div>
+      </div> */}
     </div>
   );
 }
